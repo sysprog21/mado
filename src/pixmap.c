sysprog21/mado

commit ceb748f9a69b40c98a328361ab2f752b9e1031fa

Fix heap-buffer-overflow
Resolved a heap-buffer-overflow in _twin_edge_fill by adjusting boundary
initialization. The right clip boundary was set to pixmap->width - 1,
ensuring correct clipping during rendering.

This prevents out-of-bounds memory access when filling spans and avoids
memory overflow issues during edge rendering.
 - Set pixmap->clip.right = pixmap->width - 1

src/pixmap.c
@@ -28,7 +28,7 @@ twin_pixmap_t *twin_pixmap_create(twin_format_t format,
     pixmap->height = height;
     twin_matrix_identity(&pixmap->transform);
     pixmap->clip.left = pixmap->clip.top = 0;
-    pixmap->clip.right = pixmap->width;
+    pixmap->clip.right = pixmap->width - 1;
     pixmap->clip.bottom = pixmap->height;
     pixmap->origin_x = pixmap->origin_y = 0;
     pixmap->stride = stride;
@@ -58,7 +58,7 @@ twin_pixmap_t *twin_pixmap_create_const(twin_format_t format,
     pixmap->height = height;
     twin_matrix_identity(&pixmap->transform);
     pixmap->clip.left = pixmap->clip.top = 0;
-    pixmap->clip.right = pixmap->width;
+    pixmap->clip.right = pixmap->width - 1;
     pixmap->clip.bottom = pixmap->height;
     pixmap->origin_x = pixmap->origin_y = 0;
     pixmap->stride = stride;
@@ -222,8 +222,8 @@ void twin_pixmap_clip(twin_pixmap_t *pixmap,
         pixmap->clip.left = 0;
     if (pixmap->clip.top < 0)
         pixmap->clip.top = 0;
-    if (pixmap->clip.right > pixmap->width)
-        pixmap->clip.right = pixmap->width;
+    if (pixmap->clip.right > pixmap->width - 1)
+        pixmap->clip.right = pixmap->width - 1;
     if (pixmap->clip.bottom > pixmap->height)
         pixmap->clip.bottom = pixmap->height;
 }
@@ -260,7 +260,7 @@ void twin_pixmap_reset_clip(twin_pixmap_t *pixmap)
 {
     pixmap->clip.left = 0;
     pixmap->clip.top = 0;
-    pixmap->clip.right = pixmap->width;
+    pixmap->clip.right = pixmap->width - 1;
     pixmap->clip.bottom = pixmap->height;
 }
 

commit f4137f57ef1fb9bf799622e560656f75a22b90fe

Fix display issue and heap-buffer-overflow
The issue with the display not showing the rightmost pixel of the icon
and clock is caused by commit ceb748f. Therefore, in ceb748f, solving
the heap-buffer-overflow issue by using width - 1 is incorrect. We
revert the changes from commit ceb748f and instead perform a check in
`_span_fill()`. The last pixel may have already been covered when
processing the middle pixels. To address this, an additional condition
is added when processing the last pixel to ensure it is not covered by
the middle pixels, allowing us to handle the last pixel separately.

See sysprog21#49
See sysprog21#51
Close sysprog21#78

src/pixmap.c
@@ -37,7 +37,7 @@ twin_pixmap_t *twin_pixmap_create(twin_format_t format,
     pixmap->height = height;
     twin_matrix_identity(&pixmap->transform);
     pixmap->clip.left = pixmap->clip.top = 0;
-    pixmap->clip.right = pixmap->width - 1;
+    pixmap->clip.right = pixmap->width;
     pixmap->clip.bottom = pixmap->height;
     pixmap->origin_x = pixmap->origin_y = 0;
     pixmap->stride = stride;
@@ -67,7 +67,7 @@ twin_pixmap_t *twin_pixmap_create_const(twin_format_t format,
     pixmap->height = height;
     twin_matrix_identity(&pixmap->transform);
     pixmap->clip.left = pixmap->clip.top = 0;
-    pixmap->clip.right = pixmap->width - 1;
+    pixmap->clip.right = pixmap->width;
     pixmap->clip.bottom = pixmap->height;
     pixmap->origin_x = pixmap->origin_y = 0;
     pixmap->stride = stride;
@@ -231,8 +231,8 @@ void twin_pixmap_clip(twin_pixmap_t *pixmap,
         pixmap->clip.left = 0;
     if (pixmap->clip.top < 0)
         pixmap->clip.top = 0;
-    if (pixmap->clip.right > pixmap->width - 1)
-        pixmap->clip.right = pixmap->width - 1;
+    if (pixmap->clip.right > pixmap->width)
+        pixmap->clip.right = pixmap->width;
     if (pixmap->clip.bottom > pixmap->height)
         pixmap->clip.bottom = pixmap->height;
 }
@@ -269,7 +269,7 @@ void twin_pixmap_reset_clip(twin_pixmap_t *pixmap)
 {
     pixmap->clip.left = 0;
     pixmap->clip.top = 0;
-    pixmap->clip.right = pixmap->width - 1;
+    pixmap->clip.right = pixmap->width;
     pixmap->clip.bottom = pixmap->height;
 }
 

src/poly.c
@@ -213,7 +213,7 @@ static void _span_fill(twin_pixmap_t *pixmap,
     }
 
     /* last pixel */
-    if (right & TWIN_POLY_MASK) {
+    if (right & TWIN_POLY_MASK && x != right) {
         w = 0;
         col = 0;
         while (x < right) {